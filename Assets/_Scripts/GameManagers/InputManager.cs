0558cb538e87b6dc689d35de848e6cd1ff23b59a
paperclip007 <alexbafaloukos@gmail.com>
2015-11-22 15:39:57 -0500

    Work on Overseer map

---
+++ Assets/_Scripts/GameManagers/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+    public static InputManager Instance = null;
+    public float ScrollDistance = 0.1f;
+
+    public static float OverseerZoom {
+        get { return Input.GetAxisRaw("OverseerZoom"); }
+    }
+    public static float OverseerScrollHorizontal {
+        get {
+            float input = Input.GetAxisRaw("OverseerScrollHorizontal");
+            return input;
+
+        }
+    }
+    public static float OverseerScrollVertical {
+        get {
+            return Input.GetAxisRaw("OverseerScrollVertical");
+        }
+    }
+
+    void Awake() {
+        if (!Instance) {
+            Instance = this;
+        }
+        else {
+            Destroy(this);
+        }
+    }
+	
+}
--- Assets/_Scripts/Objects/Camera/OverseerCamera.cs
+++ Assets/_Scripts/Objects/Camera/OverseerCamera.cs
@@ -7,35 +7,103 @@ public class OverseerCamera : MonoBehaviour {
 
     public Map GameMap;
     public float MaxZoom = 2;
+    public float ZoomSpeed = 0.25f;
+
+    public float Zoom = 1;
+
+    public float ScrollSpeed = 5;
 
     private Camera _camera;
-    public float _maxHeight = 10;
-    public float _minHeight = 10;
+    public float _maxHeight = 0;
+    public float _xMinBound = 0;
+    public float _xMaxBound = 0;
+    public float _zMinBound = 0;
+    public float _zMaxBound = 0;
     
     void Start() {
-        UpdateCameraPositions();
+        InitializeCamera();
+        UpdateCameraBounds();
     }
 
     void Update() {
-        if (transform.position.y > _maxHeight) {
-            transform.position = new Vector3(transform.position.x, _maxHeight, transform.position.z);
+        if (Mathf.Abs(InputManager.OverseerZoom) > float.Epsilon) {
+            Zoom += (InputManager.OverseerZoom / Mathf.Abs(InputManager.OverseerZoom)) * ZoomSpeed * Time.deltaTime;
+            if (Zoom > MaxZoom) Zoom = MaxZoom;
+            if (Zoom < 1) Zoom = 1;
         }
-        if (transform.position.y < _minHeight) {
-            transform.position = new Vector3(transform.position.x, _minHeight, transform.position.z);
+        transform.position = new Vector3(transform.position.x, _maxHeight / Zoom, transform.position.z);
+
+        UpdateCameraBounds();
+
+        if (Mathf.Abs(InputManager.OverseerScrollHorizontal) > float.Epsilon) {
+            float newX = transform.position.x + InputManager.OverseerScrollHorizontal * ScrollSpeed * Time.deltaTime;
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+        if (Mathf.Abs(InputManager.OverseerScrollVertical) > float.Epsilon) {
+            float newZ = transform.position.z + InputManager.OverseerScrollVertical * ScrollSpeed * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, transform.position.y, newZ);
+        }
+
+        if (transform.position.x > _xMaxBound) {
+            transform.position = new Vector3(_xMaxBound, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < _xMinBound) {
+            transform.position = new Vector3(_xMinBound, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z > _zMaxBound) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, _zMaxBound);
+        }
+        if (transform.position.z < _zMinBound) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, _zMinBound);
         }
     }
 
-    public void UpdateCameraPositions() {
+    public void InitializeCamera() {
         _camera = GetComponent<Camera>();
+        Zoom = 1;
         if (GameMap) {
-            float hAngle = _camera.fieldOfView / 2;
+            float vFOV = _camera.fieldOfView;
+            float radAngle = _camera.fieldOfView * Mathf.Deg2Rad;
+            float radHFOV = 2 * Mathf.Atan(Mathf.Tan(radAngle / 2) * _camera.aspect);
+            float hFOV = Mathf.Rad2Deg * radHFOV;
+            float hAngle = hFOV / 2;
+
             float mapWidth = GameMap.Terrain.GetComponent<Terrain>().terrainData.size.x;
             float mapHeight = GameMap.Terrain.GetComponent<Terrain>().terrainData.size.z;
 
-            _maxHeight = (mapWidth / 2) / Mathf.Tan(Mathf.Deg2Rad * hAngle);
-            _minHeight = (mapWidth / MaxZoom) / Mathf.Sign(Mathf.Deg2Rad * hAngle);
+            _maxHeight = ((mapWidth / 2) / Mathf.Tan(Mathf.Deg2Rad * hAngle));
 
-            transform.position = new Vector3(GameMap.transform.position.x + mapWidth / 2, GameMap.transform.position.y + _maxHeight, GameMap.transform.position.z + mapHeight / 2); 
+            transform.position = new Vector3(GameMap.transform.position.x + mapWidth / 2, GameMap.transform.position.y + _maxHeight, GameMap.transform.position.z + mapHeight / 2);
+        }
+    }
+
+    public void UpdateCameraBounds() {
+        if (GameMap) {
+            float vFOV = _camera.fieldOfView;
+            float radAngle = _camera.fieldOfView * Mathf.Deg2Rad;
+            float radHFOV = 2 * Mathf.Atan(Mathf.Tan(radAngle / 2) * _camera.aspect);
+            float hFOV = Mathf.Rad2Deg * radHFOV;
+            float vAngle = vFOV / 2;
+            float hAngle = hFOV / 2;
+
+            float mapWidth = GameMap.Terrain.GetComponent<Terrain>().terrainData.size.x;
+            float mapHeight = GameMap.Terrain.GetComponent<Terrain>().terrainData.size.z;
+            float viewWidth = (transform.position.y * Mathf.Tan(Mathf.Deg2Rad * hAngle) * 2);
+            float viewHeight = (transform.position.y * Mathf.Tan(Mathf.Deg2Rad * vAngle) * 2);
+
+            _xMinBound = GameMap.Terrain.transform.position.x + viewWidth / 2;
+            _xMaxBound = GameMap.Terrain.transform.position.x + mapWidth - viewWidth / 2;
+            _zMinBound = GameMap.Terrain.transform.position.z + viewHeight / 2;
+            _zMaxBound = GameMap.Terrain.transform.position.z + mapHeight - viewHeight / 2;
+
+            if (_xMaxBound < _xMinBound) {
+                _xMinBound = (_xMinBound + _xMaxBound) / 2;
+                _xMaxBound = _xMinBound;
+            }
+            if (_zMaxBound < _zMinBound) {
+                _zMinBound = (_zMinBound + _zMaxBound) / 2;
+                _zMaxBound = _zMinBound;
+            }
         }
     }
 }
@@ -47,8 +115,12 @@ public class UpdateOverseerCamera : Editor {
         DrawDefaultInspector();
 
         OverseerCamera cam = (OverseerCamera)target;
-        if (GUILayout.Button("Update Camera Limits")) {
-            cam.UpdateCameraPositions();
+        if (GUILayout.Button("Intialize Camera")) {
+            cam.InitializeCamera();
+            cam.UpdateCameraBounds();
+        }
+        if (GUILayout.Button("Update Camera Bounds")) {
+            cam.UpdateCameraBounds();
         }
     }
 }
