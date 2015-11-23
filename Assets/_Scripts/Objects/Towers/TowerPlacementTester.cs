9ca7e446e4acb85c7d2b3253b47be991d15f46a2
Alex Bafaloukos <alexbafaloukos@gmail.com>
2015-11-23 17:56:23 -0500

    Can now place towers

---
+++ Assets/_Scripts/Objects/Towers/TowerPlacementTester.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class TowerPlacementTester : MonoBehaviour {
+
+    public Tower TowerToPlace = null;
+    public Material ValidMaterial = null;
+    public Material InvalidMaterial = null;
+
+    private MeshFilter _meshFilter;
+    private MeshRenderer _meshRenderer;
+
+    public void Init(Tower tower) {
+        _meshFilter = GetComponent<MeshFilter>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+
+        TowerToPlace = tower;
+        _meshFilter.mesh = TowerToPlace.GetComponent<MeshFilter>().sharedMesh;
+        _meshRenderer.material = ValidMaterial;
+
+        transform.localScale = TowerToPlace.transform.localScale;
+    }
+
+    void Update() {
+        if (TowerPlacer.Instance.TowerPlaceModeOn) {
+            transform.position = TowerPlacer.Instance.TowerPlaceLocation;
+            if (TowerPlacer.Instance.TowerPlaceLocationValid) {
+                _meshRenderer.material = ValidMaterial;
+            }
+            else {
+                _meshRenderer.material = InvalidMaterial;
+            }
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaceTower() {
+        GameObject.Instantiate(TowerToPlace, transform.position, transform.rotation);
+        
+    }
+}
--- Assets/_Scripts/Player/Overseer/TowerPlacer.cs
+++ Assets/_Scripts/Player/Overseer/TowerPlacer.cs
@@ -5,7 +5,11 @@ public class TowerPlacer : MonoBehaviour {
 
     public static TowerPlacer Instance;
 
+    public GameObject TowerPlaceTesterPrefab = null;
+    public GameObject TowerPlaceTester = null;
     public bool TowerPlaceModeOn = false;
+    public bool TowerPlaceLocationValid = true;
+    public Vector3 TowerPlaceLocation;
     public GameObject TowerToPlace = null;
     public GameObject TestSphere = null;
 
@@ -27,18 +31,29 @@ public class TowerPlacer : MonoBehaviour {
             TowerPlaceModeOn = !TowerPlaceModeOn;
         }
 
+        if (TowerPlaceModeOn && Input.GetMouseButtonUp(0)) {
+            TowerPlaceTester.GetComponent<TowerPlacementTester>().PlaceTower();
+            TowerPlaceModeOn = false;
+        }
+
         if (TowerPlaceModeOn) {
-            if (TestSphere) {
+            //if (TestSphere) {
+            if (!TowerPlaceTester) {
+                TowerPlaceTester = Instantiate(TowerPlaceTesterPrefab);
+                TowerPlaceTester.GetComponent<TowerPlacementTester>().Init(TowerToPlace.GetComponent<Tower>());
+            }
+
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 LayerMask mask = LayerMask.GetMask("Map");
                 RaycastHit hit;
                 Physics.Raycast(ray, out hit, Camera.main.transform.position.y * 2, mask);
                 if (hit.collider != null) {
                     //TestSphere.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y));
-                    TestSphere.transform.position = hit.point;
+                    //TestSphere.transform.position = hit.point;
+                    TowerPlaceLocation = hit.point;
                 }
                 Debug.DrawRay(ray.origin, ray.direction * Camera.main.transform.position.y * 2, Color.green);
-            }
+            //}
         }
 	}
 }
