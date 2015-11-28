b29dae004a98cac86af277a0d37616051d92877c
Alex Bafaloukos <alexbafaloukos@gmail.com>
2015-11-28 16:37:27 -0500

    You can no longer place tower's within a certain radius of other towers (depending on the tower)

--- Assets/_Scripts/Objects/Towers/TowerPlacementTester.cs
+++ Assets/_Scripts/Objects/Towers/TowerPlacementTester.cs
@@ -5,20 +5,28 @@ using System.Collections;
 [RequireComponent(typeof(MeshRenderer))]
 public class TowerPlacementTester : MonoBehaviour {
 
+    public Map GameMap = null;
     public Tower TowerToPlace = null;
     public Material ValidMaterial = null;
     public Material InvalidMaterial = null;
 
     private MeshFilter _meshFilter;
     private MeshRenderer _meshRenderer;
+    private MapCircleDrawer _mapCircleDrawer;
 
-    public void Init(Tower tower) {
+    public void Init(Map map, Tower tower) {
+        GameMap = map;
+
         _meshFilter = GetComponent<MeshFilter>();
         _meshRenderer = GetComponent<MeshRenderer>();
+        _mapCircleDrawer = GetComponent<MapCircleDrawer>();
 
         TowerToPlace = tower;
         _meshFilter.mesh = TowerToPlace.GetComponent<MeshFilter>().sharedMesh;
         _meshRenderer.material = ValidMaterial;
+
+        _mapCircleDrawer.CircleRadius = TowerToPlace.Radius;
+        _mapCircleDrawer.GameMap = GameMap;
 
         transform.localScale = TowerToPlace.transform.localScale;
     }
@@ -32,14 +40,22 @@ public class TowerPlacementTester : MonoBehaviour {
             else {
                 _meshRenderer.material = InvalidMaterial;
             }
+            if (!_mapCircleDrawer.CircleIsVisible()) {
+                _mapCircleDrawer.SetCircleVisible(true);
+            }
+            _mapCircleDrawer.UpdateCircle();
         }
         else {
             Destroy(gameObject);
         }
     }
 
-    public void PlaceTower() {
-        GameObject.Instantiate(TowerToPlace, transform.position, transform.rotation);
-        
+    public Tower PlaceTower() {
+        Tower tower = GameObject.Instantiate<Tower>(TowerToPlace);
+        tower.transform.position = transform.position;
+        tower.transform.rotation = transform.rotation;
+        tower.GetComponent<MapCircleDrawer>().GameMap = GameMap;
+        tower.GetComponent<MapCircleDrawer>().CircleRadius = tower.Radius;
+        return tower;
     }
 }
