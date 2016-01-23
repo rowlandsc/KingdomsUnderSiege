207c5c8532285392a4e49ea1b3e7d6767f386b02
rowlandsc <cr549@drexel.edu>
2016-01-23 18:03:12 -0500

    Created a functional start scene and options screen

---
+++ Assets/_Scripts/GUI/MenuScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour
+{
+    public Canvas optionsMenu;
+    public Button playText;
+    public Button optionsText;
+    public Button quitMenuText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        optionsMenu = optionsMenu.GetComponent<Canvas>();
+        playText = playText.GetComponent<Button>();
+        optionsText = optionsText.GetComponent<Button>();
+        quitMenuText = quitMenuText.GetComponent<Button>();
+
+        optionsMenu.enabled = false;
+
+	}
+	
+    public void StartLevel()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsPress()
+    {
+        optionsMenu.enabled = true;
+    }
+
+    public void CloseMenuPress()
+    {
+        optionsMenu.enabled = false;
+    }
+}
