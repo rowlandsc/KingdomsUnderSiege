71a49aeb009798bce1e7a240f12c7732d2d555b6
rowlandsc <cr549@drexel.edu>
2016-01-29 22:15:53 -0500

    StartScreen, multi screen, options screen
    -Added our team to the start screen
    -Made menu more functional
    -Added in a multiplayer menu where you can enter in a ip address or host
    a game.
    -Changes to menuscripts

--- Assets/_Scripts/GUI/MenuScript.cs
+++ Assets/_Scripts/GUI/MenuScript.cs
@@ -1,43 +1,106 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
 public class MenuScript : MonoBehaviour
 {
-    public Canvas optionsMenu;
-    public Button playText;
-    public Button optionsText;
-    public Button quitMenuText;
+    public Button playTextBtn;
 
-	// Use this for initialization
+    public Canvas optionsMenu;
+    public Button optionsTextBtn; 
+    public Button quitOptionsMenuBtn;
+    private bool _optionsOpen = false;
+
+    public Canvas multiplayerMenu;
+    public Button hostGameBtn;
+    public Button connectToBtn;
+    public Button quitMuliBtn;
+    public InputField ipAddressField;
+    
+	/// <summary>
+    /// Use this for initialization.
+    /// </summary>
 	void Start ()
     {
+        playTextBtn = playTextBtn.GetComponent<Button>();
         optionsMenu = optionsMenu.GetComponent<Canvas>();
-        playText = playText.GetComponent<Button>();
-        optionsText = optionsText.GetComponent<Button>();
-        quitMenuText = quitMenuText.GetComponent<Button>();
-
+        optionsTextBtn = optionsTextBtn.GetComponent<Button>();
+        quitOptionsMenuBtn = quitOptionsMenuBtn.GetComponent<Button>();
         optionsMenu.enabled = false;
 
-	}
-	
-    public void StartLevel()
-    {
-        Application.LoadLevel(1);
-    }
+        multiplayerMenu = multiplayerMenu.GetComponent<Canvas>();
+        hostGameBtn = hostGameBtn.GetComponent<Button>();
+        connectToBtn = connectToBtn.GetComponent<Button>();
+        quitMuliBtn = quitMuliBtn.GetComponent<Button>();
+        multiplayerMenu.enabled = false;
+        ipAddressField = ipAddressField.GetComponent<InputField>();
 
+	}
+
+	/// <summary>
+    /// Closes the application.
+    /// </summary>
     public void ExitGame()
     {
         Application.Quit();
     }
 
+    /// <summary>
+    /// Actions for opening and closing the options button.
+    /// </summary>
     public void OptionsPress()
     {
-        optionsMenu.enabled = true;
+        if(_optionsOpen == false)
+        {
+            optionsMenu.enabled = true;
+            _optionsOpen = true;
+
+            multiplayerMenu.enabled = false;
+        }
+        else
+        {
+            optionsMenu.enabled = false;
+            _optionsOpen = false;
+        }
     }
 
+    /// <summary>
+    /// Pressed the close menu button. Closes the options menu.
+    /// </summary>
     public void CloseMenuPress()
     {
         optionsMenu.enabled = false;
+        _optionsOpen = false;   
+    }
+
+    /// <summary>
+    /// Pressed play button. Brings you to the multiplayer menu.
+    /// </summary>
+    public void PlayPress()
+    {
+        multiplayerMenu.enabled = true;
+    }
+
+    /// <summary>
+    /// Closes the multiplayer menu
+    /// </summary>
+    public void CloseMultiPress()
+    {
+        multiplayerMenu.enabled = false;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void ConnectToPress()
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void ipAddressEntered()
+    {
+
     }
 }
