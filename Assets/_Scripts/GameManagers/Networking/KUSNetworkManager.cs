ca4f51a1f72496d37e8191ea28b4d403579d3954
Alex Bafaloukos <cr549@n1-142-112.dhcp.drexel.edu>
2016-02-08 21:21:35 -0500

    Kicked networking's ass

---
+++ Assets/_Scripts/GameManagers/Networking/KUSNetworkManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class KUSNetworkManager : NetworkManager {
+
+	public int Port = 84000;
+	public InputField IPInputField;
+	public Button HostGameButton;
+	public Button JoinGameButton;
+	public Button DisconnectButton;
+
+	public void StartUpHost() {
+		SetPort();
+		NetworkManager.singleton.StartHost();
+	}
+
+	public void JoinGame() {
+		SetIPAddress();
+		SetPort();
+		NetworkManager.singleton.StartClient();
+	}
+
+	void SetIPAddress() {
+		string ip = IPInputField.text;
+		NetworkManager.singleton.networkAddress = ip;
+	}
+
+	void SetPort() {
+		NetworkManager.singleton.networkPort = Port;
+	}
+
+	void SetUpMenuSceneButtons() {
+		HostGameButton.onClick.RemoveAllListeners();
+		HostGameButton.onClick.AddListener(StartUpHost);
+		JoinGameButton.onClick.RemoveAllListeners();
+		JoinGameButton.onClick.AddListener(JoinGame);
+	}
+
+	void SetUpSetUpSceneButtons() {
+		DisconnectButton.onClick.RemoveAllListeners();
+		DisconnectButton.onClick.AddListener(NetworkManager.singleton.StopHost);
+	}
+
+	void OnLevelWasLoaded(int level) {
+		if (level == 0) {
+			HostGameButton = GameObject.Find("StartHostButton").GetComponent<Button>();
+			JoinGameButton = GameObject.Find("JoinGameButton").GetComponent<Button>();
+			IPInputField = GameObject.Find("IPInputField").GetComponent<InputField>();
+			SetUpMenuSceneButtons();
+		} else {
+			DisconnectButton = GameObject.Find("DisconnectButton").GetComponent<Button>();
+			SetUpSetUpSceneButtons();
+		}
+	}
+
+
+}
