12c9cf86ada48142497d59d687b9c872e6f51557
bfawber <bfawber@gmail.com>
2016-03-06 19:38:57 -0500

    Timer on GameScreen

---
+++ Assets/_Scripts/GUI/TimerText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerText : MonoBehaviour {
+
+    private Text _timeLeft;
+    private float _time = 0f;
+
+	// Use this for initialization
+	void Start () {
+        this._timeLeft = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this._timeLeft.text = RoundManager.Instance.CountDownTime.ToString();
+	}
+}
