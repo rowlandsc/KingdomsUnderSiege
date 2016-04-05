2bbde00cf1e339daedf4a407ffadcd2d43304081
Chenghao Wang <turnalan@qq.com>
2016-04-04 20:35:50 -0400

    fixed respawn manager.

--- Assets/_Scripts/Hero/RespawnManager.cs
+++ Assets/_Scripts/Hero/RespawnManager.cs
@@ -53,76 +53,11 @@ public class RespawnManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		cam=GameObject.FindGameObjectWithTag("MainCamera");
-	
-		if(respawn){
-			
-			herorespwn_timer-=Time.deltaTime;
-
-			herorespwn_words = "Respawn in " + ((int) Mathf.Round (herorespwn_timer)).ToString() + " seconds"; 
-
-			if(herorespwn_timer<0){
-
-
-				UIfinder = GameObject.FindGameObjectsWithTag("HeroUI");
-				for(int i=0;i<UIfinder.Length;i++){
-					Destroy(UIfinder[i]);
-				}
-
-				if(hero=="Mage(Clone)"){
-					mage_Clone=Instantiate(mage, Mage_birthplace.transform.position, Quaternion.identity)as GameObject;
-					mage_Clone.GetComponent<ProfileSystem>().inital(maxhp_,maxmp_,meleedam,seconddam,superdam,aromr_,hre_,mre_,money_);
-
-					cam.gameObject.GetComponent<TPSCamera>().ChangePlayer(mage_Clone);
-				}
-
-				if(hero=="Knight(Clone)"){
-					knight_Clone=Instantiate(knight, Knight_birthplace.transform.position, Quaternion.identity)as GameObject;
-					knight_Clone.GetComponent<ProfileSystem>().inital(maxhp_,maxmp_,meleedam,seconddam,superdam,aromr_,hre_,mre_,money_);
-
-					cam.gameObject.GetComponent<TPSCamera>().ChangePlayer(knight_Clone);
-				}
-
-				if(hero=="Arch(Clone)"){
-					arch_Clone=Instantiate(arch, Arch_birthplace.transform.position, Quaternion.identity)as GameObject;
-					arch_Clone.GetComponent<ProfileSystem>().inital(maxhp_,maxmp_,meleedam,seconddam,superdam,aromr_,hre_,mre_,money_);
-
-					cam.gameObject.GetComponent<TPSCamera>().ChangePlayer(arch_Clone);
-				}
-
-				UI_clone=Instantiate(UI, Arch_birthplace.transform.position, Quaternion.identity)as GameObject;
-				GearSystem_clone=Instantiate(GearSystem, Arch_birthplace.transform.position, Quaternion.identity)as GameObject;
-
-				respawn=false;
-				herorespwn_words=null;
-				herorespwn_timer=10f;
-
-			}
-
-		}
-
-	}
-	public void HeroDie(string heroname, float maxhp, float maxmp, float meleedamage, float seconddamage, float superdamage, float armor, float hre, float mre, float money){
-
-
-		hero= heroname;
-		maxhp_ = maxhp;
-		maxmp_ = maxmp;
-		meleedam = meleedamage;
-		seconddam = seconddamage;
-		superdam = superdamage;
-		aromr_ = armor;
-		hre_ = hre;
-		mre_ = mre;
-		money_ = money;
-
-		respawn=true;
-
+		
 
 	}
 
-	void OnGUI() {
-		GUI.Label(new Rect(Screen.width/2-Screen.width/8, Screen.height/2, 1000, 1000), herorespwn_words);
-	}
+
+
+
 }
