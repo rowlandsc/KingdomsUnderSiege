96f8fb6f417d8b9ed6370d9d909596d667bd3694
bfawber <bfawber@gmail.com>
2016-04-08 16:18:07 -0400

    Static gold functions

--- Assets/_Scripts/GameManagers/Networking/NetworkPlayerStats.cs
+++ Assets/_Scripts/GameManagers/Networking/NetworkPlayerStats.cs
@@ -34,7 +34,6 @@ public class NetworkPlayerStats : NetworkBehaviour {
     /// <summary>
     /// A function to make sure a player has enough gold to purchase things
     /// </summary>
-    /// <param name="player">The player trying to purchase things</param>
     /// <param name="amountNeeded">The amount needed to purchase the item</param>
     /// <returns>True if the player has enough, false if they don't</returns>
     public bool HasEnough(int amountNeeded)
@@ -61,5 +60,46 @@ public class NetworkPlayerStats : NetworkBehaviour {
         {
             return false;
         }
+    }
+    /// <summary>
+    /// A function to add gold to a player's gold count
+    /// </summary>
+    /// <param name="player">The player trying to purchase things</param>
+    /// <param name="amount">The amount of gold to add</param>
+    public static void AddGold(NetworkPlayerObject player, int amount)
+    {
+        player.GetComponent<NetworkPlayerStats>().AddGold(amount);
+    }
+
+    /// <summary>
+    /// A function to subtract gold from a player's gold count
+    /// </summary>
+    /// <param name="player">The player trying to purchase things</param>
+    /// <param name="amount">The amount of gold to subtract</param>
+    public static void SubtractGold(NetworkPlayerObject player, int amount)
+    {
+        player.GetComponent<NetworkPlayerStats>().SubtractGold(amount);
+    }
+
+    /// <summary>
+    /// A function to make sure a player has enough gold to purchase things
+    /// </summary>
+    /// <param name="player">The player trying to purchase things</param>
+    /// <param name="amountNeeded">The amount needed to purchase the item</param>
+    /// <returns>True if the player has enough, false if they don't</returns>
+    public static bool HasEnough(NetworkPlayerObject player, int amountNeeded)
+    {
+        return player.GetComponent<NetworkPlayerStats>().HasEnough(amountNeeded);
+    }
+
+    /// <summary>
+    /// A function to be used to make purchases
+    /// </summary>
+    /// <param name="player">The player trying to purchase things</param>
+    /// <param name="amount">The amount the thing costs</param>
+    /// <returns>True if completed, false if not</returns>
+    public static bool PurchaseItem(NetworkPlayerObject player, int amount)
+    {
+        return player.GetComponent<NetworkPlayerStats>().PurchaseItem(amount);
     }
 }
