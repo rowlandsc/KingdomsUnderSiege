d1838bc0683c25b3f80d191a266ee2ec3fcf26b0
Chenghao Wang <turnalan@qq.com>
2016-05-08 23:03:34 -0400

    End Game Condition Finished.

---
+++ Assets/_Scripts/GameManagers/EndGame.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour {
+
+	int heroWin = EndGameHelper.heroWin;
+	public Text words = null;
+
+	// Use this for initialization
+	void Start () {
+
+		if (heroWin == 1) {
+			HeroWin();
+		} else if (heroWin == 2) {
+			OverseerWin();
+		}
+	
+	}
+
+	void HeroWin() {
+		words.text = "Hero Win";
+	}
+
+	void OverseerWin() {
+		words.text = "Overseer Win";
+	}
+
+}
---
+++ Assets/_Scripts/GameManagers/EndGameHelper.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGameHelper : MonoBehaviour {
+
+	public static int heroWin;
+	public int heroWinTest;
+	private bool firsttime = true;
+
+	// Use this for initialization
+	void Start () {
+		heroWin = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		heroWinTest = heroWin;
+
+		if (firsttime == true) {
+
+			StartCoroutine(WaitForSpawnerLoad());
+			firsttime = false;
+
+		} else {
+
+			if (GameObject.Find("OverseerMinionSpawn(Clone)") == null) {
+				heroWin = 1;
+				Application.LoadLevel(3);
+			}
+				
+			if (GameObject.Find("HeroMinionSpawn(Clone)") == null) {
+				heroWin = 2;
+				Application.LoadLevel(3);
+			}
+		}
+
+	}
+
+	IEnumerator WaitForSpawnerLoad() { 
+	
+		yield return new WaitForSeconds(5);
+	
+	}
+}
