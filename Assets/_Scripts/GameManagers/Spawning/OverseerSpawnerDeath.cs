74b2aab99eba94e165f1c522894f47a10e14f17b
bfawber <bfawber@gmail.com>
2016-05-09 17:45:28 -0400

    Changes for new end conditions

--- Assets/_Scripts/GameManagers/Spawning/OverseerSpawnerDeath.cs
+++ Assets/_Scripts/GameManagers/Spawning/OverseerSpawnerDeath.cs
@@ -4,20 +4,11 @@ using UnityEngine.Networking;
 using System.Collections.Generic;
 
 public class OverseerSpawnerDeath : NetworkBehaviour, IKillable {
-
-    public bool isDead = false;
-    public int RespawnTime = 100;
-
-
-    public delegate void Died();
-    public static event Died spawnerDied;
-    public static event Died spawnerRespawned;
-
+    
 	public void OnDeath()
     {
         int goldAmount = GetComponent<ProfileSystem>().Worth;
-        if (!isDead)
-        {
+
             if (KUSNetworkManager.KnightPlayer != null)
             {
                 NetworkPlayerStats.AddGold(KUSNetworkManager.KnightPlayer, goldAmount);
@@ -31,21 +22,6 @@ public class OverseerSpawnerDeath : NetworkBehaviour, IKillable {
                 NetworkPlayerStats.AddGold(KUSNetworkManager.ArcherPlayer, goldAmount);
             }
 
-            Debug.Log("[IMPORTANT]: Switch to destroyed model");
-
-            this.isDead = true;
-            GetComponent<ProfileSystem>().baseHealthPoints = 1000f;
-            spawnerDied();
-            StartCoroutine(respawnTimer());
-        }
-    }
-
-    public IEnumerator respawnTimer()
-    {
-        yield return new WaitForSeconds(RespawnTime);
-        this.isDead = false;
-        GetComponent<ProfileSystem>().baseHealthPoints = GetComponent<ProfileSystem>().MaxHealthPoints;
-        spawnerRespawned();
-    }
-    
+            Destroy(this.gameObject);
+    }    
 }
