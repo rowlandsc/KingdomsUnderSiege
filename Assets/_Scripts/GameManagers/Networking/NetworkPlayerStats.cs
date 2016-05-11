dd11f225caf7e4f29252a53a16a0e80ac21e3ffb
bfawber <bfawber@gmail.com>
2016-05-11 18:18:52 -0400

    Gold works for everything but spawners

--- Assets/_Scripts/GameManagers/Networking/NetworkPlayerStats.cs
+++ Assets/_Scripts/GameManagers/Networking/NetworkPlayerStats.cs
@@ -119,7 +119,7 @@ public class NetworkPlayerStats : NetworkBehaviour {
     /// <param name="amount">The amount of gold to add</param>
     public void AddGold(int amount)
     {
-        KUSNetworkManager.HostPlayer.CmdUpdateGold(this.gameObject.GetComponent<NetworkIdentity>(), this.Gold + amount);
+        this.Gold += amount;
         this.TotalGoldEarned += amount;
     }
 
