5726911ba4e3fe13374c1e60c76785b54946d328
Chenghao Wang <turnalan@qq.com>
2016-05-13 13:51:08 -0400

    added mage tower bullet

--- Assets/_Scripts/Tower/MagicTowerHit.cs
+++ Assets/_Scripts/Tower/MagicTowerHit.cs
@@ -5,19 +5,19 @@ using UnityEngine.Networking;
 public class MagicTowerAttackHit : MonoBehaviour {
 
 	public GameObject Tower ;
-    private ProfileSystem towerStats;
+	private ProfileSystem towerStats;
 
 	private float kill_time;
 	private float memory_saving_timer;
 
-    public Vector3 velocity;
+	public Vector3 velocity;
 
 	// Use this for initialization
 	void Start () {
 		kill_time=10f;
 		memory_saving_timer=0f;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		memory_saving_timer+=Time.deltaTime;
@@ -26,26 +26,28 @@ public class MagicTowerAttackHit : MonoBehaviour {
 			Destroy(this.gameObject);
 		}
 
-        transform.position += velocity;
+		transform.position += velocity;
 	}
 
 	void OnTriggerEnter(Collider col){
 
 		if(col.gameObject.tag!="OverseerPlayer"){
-            /*if(col.gameObject.GetComponent<ProfileSystem>()) {
+			/*if(col.gameObject.GetComponent<ProfileSystem>()) {
                 if(col.gameObject.GetComponent<ProfileSystem>().KillAndGains(tower.GetComponent<ProfileSystem>().MeleeDamageDealt))
 				{
                     tower.GetComponent<ProfileSystem>().haveMoney+=col.gameObject.GetComponent<ProfileSystem>().Worth;
                 }
 			}*/
 
-            towerStats = Tower.GetComponent<ProfileSystem>();
-            ProfileSystem colProfile = col.gameObject.GetComponent<ProfileSystem>();
-            if (colProfile) {
-                ProfileEffect hitEffect = new ProfileEffect(Tower.GetComponent<NetworkIdentity>().netId, healthPointsAdd: -1 * towerStats.MeleeDamageDealt);
-                KUSNetworkManager.HostPlayer.CmdAddProfileEffect(col.GetComponent<NetworkIdentity>(), hitEffect);
-            }
-        }
+			towerStats = Tower.GetComponent<ProfileSystem>();
+			ProfileSystem colProfile = col.gameObject.GetComponent<ProfileSystem>();
+			if (colProfile) {
+				ProfileEffect slowdown = new ProfileEffect(Tower.GetComponent<NetworkIdentity>().netId, moveSpeedAdd: -1 * 0.5f, startingDuration: 3);
+				ProfileEffect hitEffect = new ProfileEffect(Tower.GetComponent<NetworkIdentity>().netId, healthPointsAdd: -1 * towerStats.SecondDamageDealt);
+				KUSNetworkManager.HostPlayer.CmdAddProfileEffect(col.GetComponent<NetworkIdentity>(), hitEffect);
+				KUSNetworkManager.HostPlayer.CmdAddProfileEffect(col.GetComponent<NetworkIdentity>(), slowdown);
+			}
+		}
 
 		Destroy(this.gameObject);
 	}
