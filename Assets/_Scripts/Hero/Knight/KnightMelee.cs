629e6cd8f69c0925db92f7e4a8ef02dc4a4a9e7d
Chenghao Wang <turnalan@qq.com>
2016-05-12 20:19:42 -0400

    fixed knight melee

--- Assets/_Scripts/Hero/Knight/KnightMelee.cs
+++ Assets/_Scripts/Hero/Knight/KnightMelee.cs
@@ -35,10 +35,6 @@ public class KnightMelee : MonoBehaviour {
 		{
 			canAttack = false;
 
-			sword = GameObject.Find("HeroKnightSwordV01");
-			anim = sword.gameObject.GetComponentInChildren<Animator>();
-			anim.Play("UseSword");
-
 			anim_clone = Instantiate(anim_, this.transform.position,Quaternion.Euler(0,Maincamera.transform.eulerAngles.y,0)) as GameObject;
             anim_clone.GetComponent<KnightMeleeHit>().Initialize(GetComponent<NetworkIdentity>());
 			anim_clone.transform.parent=this.gameObject.transform;
